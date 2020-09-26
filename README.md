70f494af3da11e43038a15c33fe05f95316de241
Ignat Zapolsky <izapolsky@gmail.com>
2020-09-26 21:27:07 +0100

    Local serve works. No idea why can't get rid of /pages/iggyzap

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-
-
 <ul>
   {% for post in site.posts %}
-    <li>
-      <a href="{{ post.url }}">{{post.date | date: "%Y-%m-%d"}} {{ post.title }}</a>
+    <li> 
+      <a href="{{ post.url | relative_url }}">{{post.date | date: "%Y-%m-%d"}} {{ post.title }}</a>
       {{post.excerpt}}
     </li>
   {% endfor %}
--- docker-run.sh
+++ docker-run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker run --rm --volume "${PWD}:/srv/jekyll" -v "iggyzap-jekyll-cache:/usr/local/bundle" -v "iggyzap-gems:/usr/gem" -it -p 4000:4000 jekyll/builder:4.1.0 jekyll s --incremental
+docker run --rm \
+  --volume "${PWD}:/srv/jekyll" -v "iggyzap-jekyll-cache:/usr/local/bundle" -v "iggyzap-gems:/usr/gem" \
+  -it -p 4000:4000 jekyll/builder:4.1.0 \
+  jekyll s --incremental -l -V -b /pages/iggyzap \
+  --config _config.yml,_config-local.yml 
