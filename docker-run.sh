bdf460c63b7871bc65a5763ac0b3d287a68f7e57
Ignat Zapolsky <izapolsky@gmail.com>
2020-10-26 21:00:36 +0000

    Linked design secion. GitHub Pages are using obsolete jekyll!

--- docker-run.sh
+++ docker-run.sh
@@ -3,4 +3,4 @@ docker run --rm \
   --volume "${PWD}:/srv/jekyll" -v "iggyzap-jekyll-cache:/usr/local/bundle" -v "iggyzap-gems:/usr/gem" \
   -it -p 35729:35729 -p 4000:4000 jekyll/builder:4.1.0 \
   jekyll s --incremental -l -V -b /pages/iggyzap  -w --force-polling \
-  --config _config.yml 
+  --config _config.yml,_config-local.yml
